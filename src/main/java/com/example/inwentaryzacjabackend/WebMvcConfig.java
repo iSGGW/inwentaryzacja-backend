48e1c27d3eb90533812846f5da33d4d0b1d4fd83
Kacper Borowy <borovy0@gmail.com>
2022-06-08 20:25:50 +0200

    added hibernate, jpa, mysql, model mapper removed postgresql added basic Building entity Model and Controller deleted unused files

--- src/main/java/com/example/inwentaryzacjabackend/WebMvcConfig.java
+++ src/main/java/com/example/inwentaryzacjabackend/WebMvcConfig.java
@@ -2,12 +2,10 @@ package com.example.inwentaryzacjabackend;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class WebMvcConfig extends WebSecurityConfigurerAdapter {
+public class WebMvcConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
