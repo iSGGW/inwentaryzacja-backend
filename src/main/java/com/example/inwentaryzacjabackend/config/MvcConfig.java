f478ceeb2672da41172e629df64f805b21586fc7
Kacper Borowy <borovy0@gmail.com>
2022-06-25 14:12:12 +0200

    added dependencies and cors support

--- src/main/java/com/example/inwentaryzacjabackend/config/MvcConfig.java
+++ src/main/java/com/example/inwentaryzacjabackend/config/MvcConfig.java
@@ -2,6 +2,7 @@ package com.example.inwentaryzacjabackend.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- src/main/java/com/example/inwentaryzacjabackend/controller/AuthController.java
+++ src/main/java/com/example/inwentaryzacjabackend/controller/AuthController.java
@@ -1,7 +1,7 @@
 package com.example.inwentaryzacjabackend.controller;
 
-import com.example.inwentaryzacjabackend.exception.AppException;
 import com.example.inwentaryzacjabackend.exception.ApiException;
+import com.example.inwentaryzacjabackend.exception.AppException;
 import com.example.inwentaryzacjabackend.model.User;
 import com.example.inwentaryzacjabackend.model.role.Role;
 import com.example.inwentaryzacjabackend.model.role.RoleName;
@@ -22,10 +22,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
